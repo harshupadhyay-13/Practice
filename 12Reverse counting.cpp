ebe2dd5919a3d9c9a0b0f96c53b63de7eb719c9d
Harsh Vardhan Upadhyay <73693024+harshupadhyay-13@users.noreply.github.com>
2023-11-18 00:09:21 +0530

    Add files via upload

---
+++ 12Reverse counting.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //count from n to 1
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    cout<<n-i+1<<endl;
+    return 0;
+}
---
+++ 9TwoNumberMultiply.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //multiply two numbers by input
+    int a,b;
+    cin>>a>>b;
+    cout<<a*b;
+    return 0;
+}
