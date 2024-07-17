662999f26fe22bd5093fa9036309e74a4a552f4e
Harsh Vardhan Upadhyay <73693024+harshupadhyay-13@users.noreply.github.com>
2024-07-18 01:05:08 +0530

    Create isUniqueChar.java

---
+++ isUniqueChar.java
@@ -0,0 +1,18 @@
+import java.util.*;
+// an algo to check if a string has all unique characters, what if you cannot use additional ds
+//#1 trying solve using bit manipulation
+public class Problem{
+    public static void main(String[] args) {
+        System.out.println(isUniqueChar("Hars"));
+    }
+    public static boolean isUniqueChar(String str){
+        int checker=0;
+        for(int i=0;i<str.length();i++){
+            int val=str.charAt(i) - 'a';
+            if((checker& (1<<val)) >0)
+                return false;
+            checker |= (1<< val);
+        }
+        return true;
+    }
+}
