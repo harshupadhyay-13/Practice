aa0bc32342145d87fa395267028294021e99ac5d
Harsh Vardhan Upadhyay <harshupadhyay0213@gmail.com>
2024-07-20 16:59:29 +0530

    isPrime optimised code in java
    This file gives optimized solution for prime number in log(n) time complexity. A functional code.

---
+++ isPrime.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class isPrime {
+    static boolean isPrime(int n) {
+        for (int i = 2; i*i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num = sc.nextInt();
+        boolean isPrime = true;
+        isPrime = isPrime(num);
+        if (isPrime) {
+            System.out.println("Prime");
+        }
+        else System.out.println("Non-prime");
+    }
+}
